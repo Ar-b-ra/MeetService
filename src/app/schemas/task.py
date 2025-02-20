8daafe4a1fc46aa96faa9eb19c7a3b1e4e8a2f84
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-02-20 16:48:13 +0300

    feat add response status

---
+++ src/app/schemas/task.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from .statuses import Statuses
+
+
+class StatusResponse(BaseModel):
+    status: Statuses = Field(description="Статус задачи")
+    create_time: datetime = Field(description="Время создания задачи")
+    start_time: datetime | None = Field(
+        description="Время старта задачи (может быть None, если задача ещё не началась)"
+    )
+    time_to_execute: int | None = Field(
+        description="Время выполнения задачи (может быть None, если задача ещё не завершена)"
+    )
