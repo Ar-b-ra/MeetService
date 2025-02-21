db9c51fd8be9a831dfd4f0b6b82631f7f6b4d398
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-02-21 13:53:18 +0300

    feat: add statuses and settings

--- app/settings.py
+++ app/settings.py
@@ -1,1 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    redis_host: str
+    redis_port: int
+    redis_db: int
+
+    class Config:
+        env_file = ".env"
+
+
+REDIS_SETTINGS = Settings()
 TASK_EMOUNT = 2
